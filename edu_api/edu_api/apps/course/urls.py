04a95fcdd493f2631a0b870e0a35283e128c4476
chenkw <765518301@qq.com>
2020-07-14 20:43:37 +0800

    完成课程详细页面展示

---
+++ edu_api/edu_api/apps/course/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from course import views
+
+urlpatterns = [
+    path("category/", views.CourseCategoryListAPIView.as_view()),
+    path("list/", views.CourseListAPIView.as_view()),
+    path("list_filter/", views.CourseFilterListAPIView.as_view()),
+]
